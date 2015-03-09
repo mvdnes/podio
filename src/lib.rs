9c0b6147f9ed3ca6e73ee0418c731fd1ec19480e
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-03-09 10:34:43 +0100

    Implement float writing

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,34 @@ pub trait Endianness : std::marker::MarkerTrait {
     fn int_from_target<T: std::num::Int>(val: T) -> T;
 }
 
+/// Additional integer write methods for a io::Write
+pub trait WritePodExt {
+    /// Write a usize
+    fn write_usize<T: Endianness>(&mut self, usize) -> io::Result<()>;
+    /// Write a u64
+    fn write_u64<T: Endianness>(&mut self, u64) -> io::Result<()>;
+    /// Write a u32
+    fn write_u32<T: Endianness>(&mut self, u32) -> io::Result<()>;
+    /// Write a u16
+    fn write_u16<T: Endianness>(&mut self, u16) -> io::Result<()>;
+    /// Write a u8
+    fn write_u8<T: Endianness>(&mut self, u8) -> io::Result<()>;
+    /// Write a isize
+    fn write_isize<T: Endianness>(&mut self, isize) -> io::Result<()>;
+    /// Write a i64
+    fn write_i64<T: Endianness>(&mut self, i64) -> io::Result<()>;
+    /// Write a i32
+    fn write_i32<T: Endianness>(&mut self, i32) -> io::Result<()>;
+    /// Write a i16
+    fn write_i16<T: Endianness>(&mut self, i16) -> io::Result<()>;
+    /// Write a i8
+    fn write_i8<T: Endianness>(&mut self, i8) -> io::Result<()>;
+    /// Write a f32
+    fn write_f32<T: Endianness>(&mut self, f32) -> io::Result<()>;
+    /// Write a f64
+    fn write_f64<T: Endianness>(&mut self, f64) -> io::Result<()>;
+}
+
 impl Endianness for LittleEndian {
     fn int_to_target<T: std::num::Int>(val: T) -> T {
         val.to_le()
@@ -27,20 +55,6 @@ impl Endianness for BigEndian {
     fn int_from_target<T: std::num::Int>(val: T) -> T {
         <T as std::num::Int>::from_be(val)
     }
-}
-
-/// Additional integer write methods for a io::Read
-pub trait WritePodExt {
-    /// Write a usize
-    fn write_usize<T: Endianness>(&mut self, usize) -> io::Result<()>;
-    /// Write a u64
-    fn write_u64<T: Endianness>(&mut self, u64) -> io::Result<()>;
-    /// Write a u32
-    fn write_u32<T: Endianness>(&mut self, u32) -> io::Result<()>;
-    /// Write a u16
-    fn write_u16<T: Endianness>(&mut self, u16) -> io::Result<()>;
-    /// Write a u8
-    fn write_u8<T: Endianness>(&mut self, u8) -> io::Result<()>;
 }
 
 macro_rules! val_to_buf {
@@ -84,6 +98,36 @@ impl<W: Write> WritePodExt for W {
 
     fn write_u8<T: Endianness>(&mut self, val: u8) -> io::Result<()> {
         self.write_all(&[val])
+    }
+
+    fn write_isize<T: Endianness>(&mut self, val: isize) -> io::Result<()> {
+        self.write_usize::<T>(val as usize)
+    }
+
+    fn write_i64<T: Endianness>(&mut self, val: i64) -> io::Result<()> {
+        self.write_u64::<T>(val as u64)
+    }
+
+    fn write_i32<T: Endianness>(&mut self, val: i32) -> io::Result<()> {
+        self.write_u32::<T>(val as u32)
+    }
+
+    fn write_i16<T: Endianness>(&mut self, val: i16) -> io::Result<()> {
+        self.write_u16::<T>(val as u16)
+    }
+
+    fn write_i8<T: Endianness>(&mut self, val: i8) -> io::Result<()> {
+        self.write_u8::<T>(val as u8)
+    }
+
+    fn write_f32<T: Endianness>(&mut self, val: f32) -> io::Result<()> {
+        let tval: u32 = unsafe { ::std::mem::transmute(val) };
+        self.write_u32::<T>(tval)
+    }
+
+    fn write_f64<T: Endianness>(&mut self, val: f64) -> io::Result<()> {
+        let tval: u64 = unsafe { ::std::mem::transmute(val) };
+        self.write_u64::<T>(tval)
     }
 }
 
@@ -143,5 +187,27 @@ mod test {
         writer.set_position(0);
         writer.write_u8::<LittleEndian>(0x01).unwrap();
         assert_eq!(&writer.get_ref()[0..1], &[0x01]);
+    }
+
+    #[test]
+    fn write_float() {
+        let buf: &mut [u8] = &mut [0u8; 8];
+        let mut writer = io::Cursor::new(buf);
+
+        writer.set_position(0);
+        writer.write_f32::<LittleEndian>(10.12f32).unwrap();
+        assert_eq!(&writer.get_ref()[0..4], &[0x85, 0xEB, 0x21, 0x41]);
+
+        writer.set_position(0);
+        writer.write_f32::<BigEndian>(10.12f32).unwrap();
+        assert_eq!(&writer.get_ref()[0..4], &[0x41, 0x21, 0xEB, 0x85]);
+
+        writer.set_position(0);
+        writer.write_f64::<LittleEndian>(10.12f64).unwrap();
+        assert_eq!(&writer.get_ref()[0..8], &[0x3D, 0x0A, 0xD7, 0xA3, 0x70, 0x3D, 0x24, 0x40]);
+
+        writer.set_position(0);
+        writer.write_f64::<BigEndian>(10.12f64).unwrap();
+        assert_eq!(&writer.get_ref()[0..8], &[0x40, 0x24, 0x3D, 0x70, 0xA3, 0xD7, 0x0A, 0x3D]);
     }
 }
