bb1f3b544815515e2b24ffab145f5988f5574dc2
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-07-10 14:12:39 +0200

    Improve and extend the io tests

--- tests/io.rs
+++ tests/io.rs
@@ -1,50 +1,57 @@
 extern crate podio;
 
-use std::io;
-use std::io::prelude::*;
+use std::io::{self, Read};
 use podio::ReadPodExt;
 
 struct TestReader {
-    state: u32,
+    state: Option<io::Result<usize>>,
 }
 
 impl TestReader {
-    fn new(state: u32) -> TestReader {
-        TestReader { state: state }
+    fn new(first_return: io::Result<usize>) -> TestReader {
+        TestReader { state: Some(first_return) }
     }
 
     fn get(&mut self) -> io::Result<u32> {
         self.read_u32::<podio::LittleEndian>()
     }
 
-    fn test(state: u32) -> io::Result<u32> {
-        TestReader::new(state).get()
+    fn test(first_return: io::Result<usize>) -> io::Result<u32> {
+        TestReader::new(first_return).get()
     }
 }
 
 impl Read for TestReader {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let oldstate = self.state;
-        self.state = 0;
-        match oldstate {
-            0 => {},
-            1 => return Err(io::Error::new(io::ErrorKind::Interrupted, "Interrupted")),
-            2 => return Ok(0),
-            _ => {},
+        match self.state.take().unwrap_or(Ok(1)) {
+            Ok(n) => {
+                for i in buf.iter_mut().take(n) {
+                    *i = 0xA5;
+                }
+                Ok(n)
+            },
+            e @ Err(..) => e,
         }
-        buf[0] = 0;
-        Ok(1)
     }
 }
 
 #[test]
 fn interrupted() {
     // Getting an io::ErrorKind::Interrupted should be retried, and thus succeed
-    assert_eq!(TestReader::test(1).unwrap(), 0);
+    let first_read = Err(io::Error::new(io::ErrorKind::Interrupted, "Interrupted"));
+    assert_eq!(TestReader::test(first_read).unwrap(), 0xA5A5A5A5);
 }
 
 #[test]
 fn eof() {
     // Getting a Ok(0) implies an unexpected EOF
-    assert!(TestReader::test(2).is_err());
+    let first_read = Ok(0);
+    assert!(TestReader::test(first_read).is_err());
+}
+
+#[test]
+fn err() {
+    // Getting any other error is still an error
+    let first_read = Err(io::Error::new(io::ErrorKind::Other, "Other"));
+    assert!(TestReader::test(first_read).is_err());
 }
